34027f590c3e6d34569fef3f34ea34a36fd70bdc
Bryan Portillo <bportillo701@gmail.com>
2025-11-27 21:17:41 -0600

    feat(Documentation): added missing swagger documentation

--- src/json-extractor/json-extractor.controller.ts
+++ src/json-extractor/json-extractor.controller.ts
@@ -1,12 +1,33 @@
 import { Controller, Get, Query } from '@nestjs/common';
-
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JsonExtractorService } from './services/json-extractor.service';
 
+@ApiTags('JSON Extractor')
 @Controller('json-extractor')
 export class JsonExtractorController {
   constructor(private readonly jsonExtractorService: JsonExtractorService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Parse email file',
+    description: 'Extracts JSON content from a specified email file',
+  })
+  @ApiQuery({
+    name: 'file',
+    required: true,
+    type: String,
+    description: 'The name of the email file to parse',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful response',
+    schema: {
+      type: 'object',
+      description: 'The extracted JSON content from the email file',
+    },
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   parseEmail(@Query('file') query: string) {
     return this.jsonExtractorService.parseEmail(query);
   }
